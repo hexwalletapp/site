91ce5fa084aaf7ae07068b27f7b14a9001d77208
Joe Blau <im@joeblau.com>
2021-10-31 12:02:16 -0700

    Remove beta magic and just send link

--- src/components/Beta.tsx
+++ src/components/Beta.tsx
@@ -8,31 +8,31 @@ export default function Beta() {
   const [open, setOpen] = useState(false);
   const [submitted, setSubmitted] = useState(false);
 
-  const joinBeta = async (event: any) => {
-    event.preventDefault();
+  // const joinBeta = async (event: any) => {
+  //   event.preventDefault();
 
-    if (executeRecaptcha && !executeRecaptcha("beta_sign_up")) {
-      return;
-    }
+  //   if (executeRecaptcha && !executeRecaptcha("beta_sign_up")) {
+  //     return;
+  //   }
 
-    try {
-      const res = await fetch("/api/testflight", {
-        body: JSON.stringify({
-          firstName: event.target.firstName.value,
-          lastName: event.target.lastName.value,
-          email: event.target.email.value,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      });
-      setSubmitted(true);
-      setOpen(true);
-    } catch (error) {
-      alert(error);
-    }
-  };
+  //   try {
+  //     const res = await fetch("/api/testflight", {
+  //       body: JSON.stringify({
+  //         firstName: event.target.firstName.value,
+  //         lastName: event.target.lastName.value,
+  //         email: event.target.email.value,
+  //       }),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       method: "POST",
+  //     });
+  //     setSubmitted(true);
+  //     setOpen(true);
+  //   } catch (error) {
+  //     alert(error);
+  //   }
+  // };
 
   return (
     <div className="bg-white py-16 sm:py-24">
@@ -54,71 +54,11 @@ export default function Beta() {
                 </p>
               </div>
               <div className="mt-12 sm:mx-auto sm:max-w-lg sm:flex">
-                <form
-                  onSubmit={joinBeta}
-                  className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
-                >
-                  <div>
-                    <label
-                      htmlFor="firstName"
-                      className="block text-sm font-medium text-gray-100"
-                    >
-                      First name
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        type="text"
-                        name="firstName"
-                        id="firstName"
-                        className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                        placeholder="First name"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="lastName"
-                      className="block text-sm font-medium text-gray-100"
-                    >
-                      Last name
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        type="text"
-                        name="lastName"
-                        id="lastName"
-                        className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                        placeholder="Last name"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-100"
-                    >
-                      Email
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                        placeholder="Email"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <button
-                      type="submit"
-                      className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-500 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
-                      disabled={submitted}
-                    >
-                      Join Beta
-                    </button>
-                  </div>
-                </form>
+                <Link href="https://testflight.apple.com/join/iLl3I9g4">
+                  <a className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-500 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50">
+                    Join Beta
+                  </a>
+                </Link>
               </div>
               <div className="sm:text-center">
                 <p className="mt-6 mx-auto max-w-2xl text-lg text-white">
